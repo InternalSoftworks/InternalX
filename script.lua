bb83147b9447e8b95c2224778cfa7d930bfd27c3
InternalSoftworks <106435173+InternalSoftworks@users.noreply.github.com>
2022-05-28 16:17:16 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,13 @@
+local url = ""
 
+local games = {
+    [5602055394] = "Hood Modded";
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+local name = games[game.PlaceId] or games[game.GameId]
+
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
